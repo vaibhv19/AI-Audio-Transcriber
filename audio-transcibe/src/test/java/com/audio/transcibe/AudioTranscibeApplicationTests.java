93816ea9044579b784df4fea14c96fc0847ef787
Vaibhav Gupta <vaibhavkrgupta369@gmail.com>
2026-04-18 10:24:31 +0530

    project setup done

---
+++ audio-transcibe/src/test/java/com/audio/transcibe/AudioTranscibeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.audio.transcibe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudioTranscibeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
