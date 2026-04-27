5447040dc408a3743b94c2ee2038d106f1b28ffb
Vaibhav Gupta <vaibhavkrgupta369@gmail.com>
2026-04-27 07:40:58 +0530

    configured POM.xml

--- audio-transcribe/src/test/java/com/audio/transcribe/AudioTranscribeApplicationTests.java
+++ audio-transcribe/src/test/java/com/audio/transcribe/AudioTranscribeApplicationTests.java
@@ -1,10 +1,10 @@
-package com.audio.transcibe;
+package com.audio.transcribe;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class AudioTranscibeApplicationTests {
+class AudioTranscribeApplicationTests {
 
 	@Test
 	void contextLoads() {
